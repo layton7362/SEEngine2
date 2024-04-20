09baab7d01b9f19587be1f258516991301aca058
ahmed <ahmed.ahmed@web.de>
2024-04-20 13:47:15 +0200

    FIx

--- src/Engine/RenderEngine.cpp
+++ src/Engine/RenderEngine.cpp
@@ -26,10 +26,10 @@ void OpenGLRenderEngine::buildMesh(Object3D *obj)
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * vertices.size(), reinterpret_cast<float*>(vertices.data()), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * indices.size(), indices.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
@@ -44,18 +44,16 @@ void OpenGLRenderEngine::renderBegin()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 }
+
 void OpenGLRenderEngine::render()
 {
     for (auto &&id : this->render_id)
     {
         id.first->loadUniforms();
         RenderData *data = id.second;
-        glBindVertexArray(data->VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glBindVertexArray(data->VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     }
-
-    // glDrawArrays(GL_TRIANGLES, 0, 6);
-    // glBindVertexArray(0); // no need to unbind it every time
 }
 
 void OpenGLRenderEngine::renderEnd()
